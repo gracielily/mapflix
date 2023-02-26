cf3a511888c073bb36b66095d71ed7ef2f20f17a
gracelily <12722041@mail.wit.ie>
2023-02-26 11:30:04 +0000

    Add User Model tests

---
+++ test/fixtures.js
@@ -0,0 +1,32 @@
+export const testUser = {
+    firstName: "Jane",
+    lastName: "Doe",
+    email: "jane@doe.com",
+    password: "somesecret",
+    isAdmin: false,
+  };
+  
+  export const testUsers = [
+    {
+      firstName: "Homer",
+      lastName: "Simpson",
+      email: "homer@simpson.com",
+      password: "secret",
+      isAdmin: true,
+    },
+    {
+      firstName: "Marge",
+      lastName: "Simpson",
+      email: "marge@simpson.com",
+      password: "secret",
+      isAdmin: false,
+    },
+    {
+      firstName: "Bart",
+      lastName: "Simpson",
+      email: "bart@simpson.com",
+      password: "secret",
+      isAdmin: false,
+    }
+  ];
+  
